b4125a6131a51e3c3e8d0f7c78013fc715b8bf7f
choi yorim <oliv.lim01@gmail.com>
2018-10-18 12:26:25 +0900

    branch1 version1

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int number;
+            string name;
             Console.WriteLine("hello!");
             
         }
